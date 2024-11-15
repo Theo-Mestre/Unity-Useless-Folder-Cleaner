e62549dc045deb4ba3aa036f82a87c8bca5ee6bb
Theo-gamedev <g.mestretheo@gmail.com>
2024-11-15 07:29:34 +0100

    Added obj and .vs folders to be deleted

--- UnityCleaner.bat
+++ UnityCleaner.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rmdir /s /q  .vs
+rmdir /s /q  obj
 rmdir /s /q  Library
 rmdir /s /q  Logs
 rmdir /s /q  Temp
@@ -9,3 +11,5 @@ del /S *.sln
 del /S *.csproj
 del /S *.vsconfig
 del /S *.vcxproj.user
+
+pause
